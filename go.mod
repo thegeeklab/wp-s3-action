bcfd874890c0cf2780adbfab2939ab2b3e151fad
Robert Kaussow <mail@thegeeklab.de>
2024-05-11 11:08:34 +0200

    docs: hide system flags and refactor generator (#112)

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/aws/aws-sdk-go v1.51.25
 	github.com/rs/zerolog v1.32.0
 	github.com/ryanuber/go-glob v1.0.0
-	github.com/thegeeklab/wp-plugin-go v1.7.1
+	github.com/thegeeklab/wp-plugin-go/v2 v2.3.1
 	github.com/urfave/cli/v2 v2.27.2
 )
 
@@ -28,7 +28,7 @@ require (
 	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,8 +1,14 @@
 package plugin
 
 import (
-	wp "github.com/thegeeklab/wp-plugin-go/plugin"
+	"fmt"
+
+	wp "github.com/thegeeklab/wp-plugin-go/v2/plugin"
+	"github.com/thegeeklab/wp-plugin-go/v2/types"
+	"github.com/urfave/cli/v2"
 )
+
+//go:generate go run ../internal/doc/main.go -output=../docs/data/data-raw.yaml
 
 // Plugin implements provide the plugin implementation.
 type Plugin struct {
@@ -45,13 +51,172 @@ type Result struct {
 	err error
 }
 
-func New(options wp.Options, settings *Settings) *Plugin {
-	p := &Plugin{}
+func New(e wp.ExecuteFunc, build ...string) *Plugin {
+	p := &Plugin{
+		Settings: &Settings{},
+	}
 
-	options.Execute = p.run
+	options := wp.Options{
+		Name:                "wp-s3-action",
+		Description:         "Perform S3 actions",
+		Flags:               Flags(p.Settings, wp.FlagsPluginCategory),
+		Execute:             p.run,
+		HideWoodpeckerFlags: true,
+	}
+
+	if len(build) > 0 {
+		options.Version = build[0]
+	}
+
+	if len(build) > 1 {
+		options.VersionMetadata = fmt.Sprintf("date=%s", build[1])
+	}
+
+	if e != nil {
+		options.Execute = e
+	}
 
 	p.Plugin = wp.New(options)
-	p.Settings = settings
 
 	return p
+}
+
+// Flags returns a slice of CLI flags for the plugin.
+func Flags(settings *Settings, category string) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "endpoint",
+			Usage:       "endpoint for the s3 connection",
+			EnvVars:     []string{"PLUGIN_ENDPOINT", "S3_ENDPOINT"},
+			Destination: &settings.Endpoint,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "access-key",
+			Usage:       "s3 access key",
+			EnvVars:     []string{"PLUGIN_ACCESS_KEY", "S3_ACCESS_KEY"},
+			Destination: &settings.AccessKey,
+			Required:    true,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "secret-key",
+			Usage:       "s3 secret key",
+			EnvVars:     []string{"PLUGIN_SECRET_KEY", "S3_SECRET_KEY"},
+			Destination: &settings.SecretKey,
+			Required:    true,
+			Category:    category,
+		},
+		&cli.BoolFlag{
+			Name:        "path-style",
+			Usage:       "enable path style for bucket paths",
+			EnvVars:     []string{"PLUGIN_PATH_STYLE"},
+			Destination: &settings.PathStyle,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "bucket",
+			Usage:       "name of the bucket",
+			EnvVars:     []string{"PLUGIN_BUCKET"},
+			Destination: &settings.Bucket,
+			Required:    true,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "region",
+			Usage:       "s3 region",
+			Value:       "us-east-1",
+			EnvVars:     []string{"PLUGIN_REGION"},
+			Destination: &settings.Region,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "source",
+			Usage:       "upload source path",
+			Value:       ".",
+			EnvVars:     []string{"PLUGIN_SOURCE"},
+			Destination: &settings.Source,
+			Category:    category,
+		},
+		&cli.StringFlag{
+			Name:        "target",
+			Usage:       "upload target path",
+			Value:       "/",
+			EnvVars:     []string{"PLUGIN_TARGET"},
+			Destination: &settings.Target,
+			Category:    category,
+		},
+		&cli.BoolFlag{
+			Name:        "delete",
+			Usage:       "delete locally removed files from the target",
+			EnvVars:     []string{"PLUGIN_DELETE"},
+			Destination: &settings.Delete,
+			Category:    category,
+		},
+		&cli.GenericFlag{
+			Name:     "acl",
+			Usage:    "access control list",
+			EnvVars:  []string{"PLUGIN_ACL"},
+			Value:    &types.StringMapFlag{},
+			Category: category,
+		},
+		&cli.GenericFlag{
+			Name:     "content-type",
+			Usage:    "content-type settings for uploads",
+			EnvVars:  []string{"PLUGIN_CONTENT_TYPE"},
+			Value:    &types.StringMapFlag{},
+			Category: category,
+		},
+		&cli.GenericFlag{
+			Name:     "content-encoding",
+			Usage:    "content-encoding settings for uploads",
+			EnvVars:  []string{"PLUGIN_CONTENT_ENCODING"},
+			Value:    &types.StringMapFlag{},
+			Category: category,
+		},
+		&cli.GenericFlag{
+			Name:     "cache-control",
+			Usage:    "cache-control settings for uploads",
+			EnvVars:  []string{"PLUGIN_CACHE_CONTROL"},
+			Value:    &types.StringMapFlag{},
+			Category: category,
+		},
+		&cli.GenericFlag{
+			Name:     "metadata",
+			Usage:    "additional metadata for uploads",
+			EnvVars:  []string{"PLUGIN_METADATA"},
+			Value:    &types.DeepStringMapFlag{},
+			Category: category,
+		},
+		&cli.GenericFlag{
+			Name:     "redirects",
+			Usage:    "redirects to create",
+			EnvVars:  []string{"PLUGIN_REDIRECTS"},
+			Value:    &types.MapFlag{},
+			Category: category,
+		},
+		&cli.StringFlag{
+			Name:        "cloudfront-distribution",
+			Usage:       "ID of cloudfront distribution to invalidate",
+			EnvVars:     []string{"PLUGIN_CLOUDFRONT_DISTRIBUTION"},
+			Destination: &settings.CloudFrontDistribution,
+			Category:    category,
+		},
+		&cli.BoolFlag{
+			Name:        "dry-run",
+			Usage:       "dry run disables api calls",
+			EnvVars:     []string{"DRY_RUN", "PLUGIN_DRY_RUN"},
+			Destination: &settings.DryRun,
+			Category:    category,
+		},
+		&cli.IntFlag{
+			Name:  "max-concurrency",
+			Usage: "customize number concurrent files to process",
+			//nolint:gomnd
+			Value:       100,
+			EnvVars:     []string{"PLUGIN_MAX_CONCURRENCY"},
+			Destination: &settings.MaxConcurrency,
+			Category:    category,
+		},
+	}
 }
