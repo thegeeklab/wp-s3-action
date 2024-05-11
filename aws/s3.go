d1efaa54077ae2f337b40fb9f364584e650d2aed
Robert Kaussow <mail@thegeeklab.de>
2024-05-11 22:51:02 +0200

    split monolithic upload function into smaller parts

--- aws/s3.go
+++ aws/s3.go
@@ -15,7 +15,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	s3_types "github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/rs/zerolog/log"
 )
 
@@ -67,15 +67,10 @@ func NewClient(ctx context.Context, url, region, accessKey, secretKey string, pa
 	}, nil
 }
 
-// Upload uploads a file to an S3 bucket with the specified options.
-// It first checks if the file already exists in the bucket and compares the metadata.
-// If the metadata has changed, it updates the file in the bucket.
-// Otherwise, it skips the upload.
-// If the file does not exist in the bucket, it uploads the file with the specified metadata.
-// The function returns an error if the upload or update fails.
-//
-//nolint:gocognit,gocyclo,maintidx
-//nolint:gocognit,gocyclo,maintidx
+// Upload uploads a file to an S3 bucket. It first checks if the file already exists in the
+// bucket and if the content and metadata have not changed. If the file has not changed,
+// it skips the upload and just updates the metadata. If the file has changed, it uploads
+// the new file to the bucket.
 func (u *S3Uploader) Upload() error {
 	if u.Opt.Local == "" {
 		return nil
@@ -85,79 +80,21 @@ func (u *S3Uploader) Upload() error {
 	if err != nil {
 		return err
 	}
-
 	defer file.Close()
 
-	var acl string
-
-	for pattern := range u.Opt.ACL {
-		if match, _ := filepath.Match(pattern, u.Opt.Local); match {
-			acl = u.Opt.ACL[pattern]
-
-			break
-		}
-	}
-
-	if acl == "" {
-		acl = "private"
-	}
-
-	fileExt := filepath.Ext(u.Opt.Local)
-
-	var contentType string
-
-	for patternExt := range u.Opt.ContentType {
-		if patternExt == fileExt {
-			contentType = u.Opt.ContentType[patternExt]
-
-			break
-		}
-	}
-
-	if contentType == "" {
-		contentType = mime.TypeByExtension(fileExt)
-	}
-
-	var contentEncoding string
-
-	for patternExt := range u.Opt.ContentEncoding {
-		if patternExt == fileExt {
-			contentEncoding = u.Opt.ContentEncoding[patternExt]
-
-			break
-		}
-	}
-
-	var cacheControl string
-
-	for pattern := range u.Opt.CacheControl {
-		if match, _ := filepath.Match(pattern, u.Opt.Local); match {
-			cacheControl = u.Opt.CacheControl[pattern]
-
-			break
-		}
-	}
-
-	metadata := map[string]string{}
-
-	for pattern := range u.Opt.Metadata {
-		if match, _ := filepath.Match(pattern, u.Opt.Local); match {
-			for k, v := range u.Opt.Metadata[pattern] {
-				metadata[k] = v
-			}
-
-			break
-		}
-	}
-
-	var noSuchKeyErr *s3_types.NoSuchKey
+	acl := getACL(u.Opt.Local, u.Opt.ACL)
+	contentType := getContentType(u.Opt.Local, u.Opt.ContentType)
+	contentEncoding := getContentEncoding(u.Opt.Local, u.Opt.ContentEncoding)
+	cacheControl := getCacheControl(u.Opt.Local, u.Opt.CacheControl)
+	metadata := getMetadata(u.Opt.Local, u.Opt.Metadata)
 
 	head, err := u.client.HeadObject(context.TODO(), &s3.HeadObjectInput{
-		Bucket: aws.String(u.Opt.Bucket),
-		Key:    aws.String(u.Opt.Remote),
+		Bucket: &u.Opt.Bucket,
+		Key:    &u.Opt.Remote,
 	})
 	if err != nil {
-		if !errors.As(err, &noSuchKeyErr) {
+		var noSuchKeyError *types.NoSuchKey
+		if !errors.As(err, &noSuchKeyError) {
 			return err
 		}
 
@@ -168,29 +105,20 @@ func (u *S3Uploader) Upload() error {
 			acl,
 		)
 
-		putObject := &s3.PutObjectInput{
-			Bucket:      aws.String(u.Opt.Bucket),
-			Key:         aws.String(u.Opt.Remote),
-			Body:        file,
-			ContentType: aws.String(contentType),
-			ACL:         s3_types.ObjectCannedACL(acl),
-			Metadata:    metadata,
-		}
-
-		if len(cacheControl) > 0 {
-			putObject.CacheControl = aws.String(cacheControl)
-		}
-
-		if len(contentEncoding) > 0 {
-			putObject.ContentEncoding = aws.String(contentEncoding)
-		}
-
-		// skip upload during dry run
 		if u.Opt.DryRun {
 			return nil
 		}
 
-		_, err = u.client.PutObject(context.TODO(), putObject)
+		_, err = u.client.PutObject(context.TODO(), &s3.PutObjectInput{
+			Bucket:          &u.Opt.Bucket,
+			Key:             &u.Opt.Remote,
+			Body:            file,
+			ContentType:     &contentType,
+			ACL:             types.ObjectCannedACL(acl),
+			Metadata:        metadata,
+			CacheControl:    &cacheControl,
+			ContentEncoding: &contentEncoding,
+		})
 
 		return err
 	}
@@ -200,135 +128,31 @@ func (u *S3Uploader) Upload() error {
 	_, _ = io.Copy(hash, file)
 	sum := fmt.Sprintf("'%x'", hash.Sum(nil))
 
-	//nolint:nestif
 	if sum == *head.ETag {
-		shouldCopy := false
-
-		if head.ContentType == nil && contentType != "" {
-			log.Debug().Msgf("content-type has changed from unset to %s", contentType)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && head.ContentType != nil && contentType != *head.ContentType {
-			log.Debug().Msgf("content-type has changed from %s to %s", *head.ContentType, contentType)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && head.ContentEncoding == nil && contentEncoding != "" {
-			log.Debug().Msgf("Content-Encoding has changed from unset to %s", contentEncoding)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && head.ContentEncoding != nil && contentEncoding != *head.ContentEncoding {
-			log.Debug().Msgf("Content-Encoding has changed from %s to %s", *head.ContentEncoding, contentEncoding)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && head.CacheControl == nil && cacheControl != "" {
-			log.Debug().Msgf("cache-control has changed from unset to %s", cacheControl)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && head.CacheControl != nil && cacheControl != *head.CacheControl {
-			log.Debug().Msgf("cache-control has changed from %s to %s", *head.CacheControl, cacheControl)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && len(head.Metadata) != len(metadata) {
-			log.Debug().Msgf("count of metadata values has changed for %s", u.Opt.Local)
-
-			shouldCopy = true
-		}
-
-		if !shouldCopy && len(metadata) > 0 {
-			for k, v := range metadata {
-				if hv, ok := head.Metadata[k]; ok {
-					if v != hv {
-						log.Debug().Msgf("metadata values have changed for %s", u.Opt.Local)
-
-						shouldCopy = true
-
-						break
-					}
-				}
-			}
-		}
-
-		if !shouldCopy {
-			grant, err := u.client.GetObjectAcl(context.TODO(), &s3.GetObjectAclInput{
-				Bucket: aws.String(u.Opt.Bucket),
-				Key:    aws.String(u.Opt.Remote),
-			})
-			if err != nil {
-				return err
-			}
-
-			previousACL := "private"
-
-			for _, grant := range grant.Grants {
-				grantee := *grant.Grantee
-				if grantee.URI != nil {
-					if *grantee.URI == "http://acs.amazonaws.com/groups/global/AllUsers" {
-						if grant.Permission == "READ" {
-							previousACL = "public-read"
-						} else if grant.Permission == "WRITE" {
-							previousACL = "public-read-write"
-						}
-					}
-
-					if *grantee.URI == "http://acs.amazonaws.com/groups/global/AuthenticatedUsers" {
-						if grant.Permission == "READ" {
-							previousACL = "authenticated-read"
-						}
-					}
-				}
-			}
-
-			if previousACL != acl {
-				log.Debug().Msgf("permissions for '%s' have changed from '%s' to '%s'", u.Opt.Remote, previousACL, acl)
-
-				shouldCopy = true
-			}
-		}
-
+		shouldCopy, reason := u.shouldCopyObject(head, contentType, acl, contentEncoding, cacheControl, metadata)
 		if !shouldCopy {
 			log.Debug().Msgf("skipping '%s' because hashes and metadata match", u.Opt.Local)
 
 			return nil
 		}
 
-		log.Debug().Msgf("updating metadata for '%s' content-type: '%s', ACL: '%s'", u.Opt.Local, contentType, acl)
+		log.Debug().Msgf("updating metadata for '%s' %s", u.Opt.Local, reason)
 
-		copyObject := &s3.CopyObjectInput{
-			Bucket:            aws.String(u.Opt.Bucket),
-			Key:               aws.String(u.Opt.Remote),
-			CopySource:        aws.String(fmt.Sprintf("%s/%s", u.Opt.Bucket, u.Opt.Remote)),
-			ACL:               s3_types.ObjectCannedACL(acl),
-			ContentType:       aws.String(contentType),
-			Metadata:          metadata,
-			MetadataDirective: s3_types.MetadataDirectiveReplace,
-		}
-
-		if len(cacheControl) > 0 {
-			copyObject.CacheControl = aws.String(cacheControl)
-		}
-
-		if len(contentEncoding) > 0 {
-			copyObject.ContentEncoding = aws.String(contentEncoding)
-		}
-
-		// skip update if dry run
 		if u.Opt.DryRun {
 			return nil
 		}
 
-		_, err = u.client.CopyObject(context.Background(), copyObject)
+		_, err = u.client.CopyObject(context.Background(), &s3.CopyObjectInput{
+			Bucket:            &u.Opt.Bucket,
+			Key:               &u.Opt.Remote,
+			CopySource:        aws.String(fmt.Sprintf("%s/%s", u.Opt.Bucket, u.Opt.Remote)),
+			ACL:               types.ObjectCannedACL(acl),
+			ContentType:       &contentType,
+			Metadata:          metadata,
+			MetadataDirective: types.MetadataDirectiveReplace,
+			CacheControl:      &cacheControl,
+			ContentEncoding:   &contentEncoding,
+		})
 
 		return err
 	}
@@ -340,29 +164,181 @@ func (u *S3Uploader) Upload() error {
 
 	log.Debug().Msgf("uploading '%s' with content-type '%s' and permissions '%s'", u.Opt.Local, contentType, acl)
 
-	putObject := &s3.PutObjectInput{
-		Bucket:      aws.String(u.Opt.Bucket),
-		Key:         aws.String(u.Opt.Remote),
-		Body:        file,
-		ContentType: aws.String(contentType),
-		ACL:         s3_types.ObjectCannedACL(acl),
-		Metadata:    metadata,
-	}
-
-	if len(cacheControl) > 0 {
-		putObject.CacheControl = aws.String(cacheControl)
-	}
-
-	if len(contentEncoding) > 0 {
-		putObject.ContentEncoding = aws.String(contentEncoding)
-	}
-
-	// skip upload if dry run
 	if u.Opt.DryRun {
 		return nil
 	}
 
-	_, err = u.client.PutObject(context.TODO(), putObject)
+	_, err = u.client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:          &u.Opt.Bucket,
+		Key:             &u.Opt.Remote,
+		Body:            file,
+		ContentType:     &contentType,
+		ACL:             types.ObjectCannedACL(acl),
+		Metadata:        metadata,
+		CacheControl:    &cacheControl,
+		ContentEncoding: &contentEncoding,
+	})
 
 	return err
+}
+
+// shouldCopyObject determines whether an S3 object should be copied based on changes in content type,
+// content encoding, cache control, and metadata. It compares the existing object's metadata with the
+// provided metadata and returns a boolean indicating whether the object should be copied,
+// along with a string describing the reason for the copy if applicable.
+//
+//nolint:gocognit
+func (u *S3Uploader) shouldCopyObject(
+	head *s3.HeadObjectOutput, contentType, acl, contentEncoding, cacheControl string, metadata map[string]string,
+) (bool, string) {
+	var reason string
+
+	if head.ContentType == nil && contentType != "" {
+		reason = fmt.Sprintf("content-type has changed from unset to %s", contentType)
+
+		return true, reason
+	}
+
+	if head.ContentType != nil && contentType != *head.ContentType {
+		reason = fmt.Sprintf("content-type has changed from %s to %s", *head.ContentType, contentType)
+
+		return true, reason
+	}
+
+	if head.ContentEncoding == nil && contentEncoding != "" {
+		reason = fmt.Sprintf("Content-Encoding has changed from unset to %s", contentEncoding)
+
+		return true, reason
+	}
+
+	if head.ContentEncoding != nil && contentEncoding != *head.ContentEncoding {
+		reason = fmt.Sprintf("Content-Encoding has changed from %s to %s", *head.ContentEncoding, contentEncoding)
+
+		return true, reason
+	}
+
+	if head.CacheControl == nil && cacheControl != "" {
+		reason = fmt.Sprintf("cache-control has changed from unset to %s", cacheControl)
+
+		return true, reason
+	}
+
+	if head.CacheControl != nil && cacheControl != *head.CacheControl {
+		reason = fmt.Sprintf("cache-control has changed from %s to %s", *head.CacheControl, cacheControl)
+
+		return true, reason
+	}
+
+	if len(head.Metadata) != len(metadata) {
+		reason = fmt.Sprintf("count of metadata values has changed for %s", u.Opt.Local)
+
+		return true, reason
+	}
+
+	if len(metadata) > 0 {
+		for k, v := range metadata {
+			if hv, ok := head.Metadata[k]; ok {
+				if v != hv {
+					reason = fmt.Sprintf("metadata values have changed for %s", u.Opt.Local)
+
+					return true, reason
+				}
+			}
+		}
+	}
+
+	grant, err := u.client.GetObjectAcl(context.TODO(), &s3.GetObjectAclInput{
+		Bucket: &u.Opt.Bucket,
+		Key:    &u.Opt.Remote,
+	})
+	if err != nil {
+		return false, ""
+	}
+
+	previousACL := "private"
+
+	for _, g := range grant.Grants {
+		grantee := g.Grantee
+		if grantee.URI != nil {
+			switch *grantee.URI {
+			case "http://acs.amazonaws.com/groups/global/AllUsers":
+				if g.Permission == "READ" {
+					previousACL = "public-read"
+				} else if g.Permission == "WRITE" {
+					previousACL = "public-read-write"
+				}
+			case "http://acs.amazonaws.com/groups/global/AuthenticatedUsers":
+				if g.Permission == "READ" {
+					previousACL = "authenticated-read"
+				}
+			}
+		}
+	}
+
+	if previousACL != acl {
+		reason = fmt.Sprintf("permissions for '%s' have changed from '%s' to '%s'", u.Opt.Remote, previousACL, acl)
+
+		return true, reason
+	}
+
+	return false, ""
+}
+
+// getACL returns the ACL for the given file based on the provided patterns.
+func getACL(file string, patterns map[string]string) string {
+	for pattern, acl := range patterns {
+		if match, _ := filepath.Match(pattern, file); match {
+			return acl
+		}
+	}
+
+	return "private"
+}
+
+// getContentType returns the content type for the given file based on the provided patterns.
+func getContentType(file string, patterns map[string]string) string {
+	ext := filepath.Ext(file)
+	if contentType, ok := patterns[ext]; ok {
+		return contentType
+	}
+
+	return mime.TypeByExtension(ext)
+}
+
+// getContentEncoding returns the content encoding for the given file based on the provided patterns.
+func getContentEncoding(file string, patterns map[string]string) string {
+	ext := filepath.Ext(file)
+	if contentEncoding, ok := patterns[ext]; ok {
+		return contentEncoding
+	}
+
+	return ""
+}
+
+// getCacheControl returns the cache control for the given file based on the provided patterns.
+func getCacheControl(file string, patterns map[string]string) string {
+	for pattern, cacheControl := range patterns {
+		if match, _ := filepath.Match(pattern, file); match {
+			return cacheControl
+		}
+	}
+
+	return ""
+}
+
+// getMetadata returns the metadata for the given file based on the provided patterns.
+func getMetadata(file string, patterns map[string]map[string]string) map[string]string {
+	metadata := make(map[string]string)
+
+	for pattern, meta := range patterns {
+		if match, _ := filepath.Match(pattern, file); match {
+			for k, v := range meta {
+				metadata[k] = v
+			}
+
+			break
+		}
+	}
+
+	return metadata
 }
